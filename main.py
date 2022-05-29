c7fd5a989c23304716912c21a0d62fc2ade408b2
Moredy <matheusmarques1902@gmail.com>
2022-05-29 15:02:58 -0300

    Estrutura do projeto

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
+import pandas as pd
 from classes.Person import Person
+from mapas import mapa01
 
 person = Person('Felipe' , 12);
+
+df = pd.read_csv ('./mapas/mapa_01.csv')
+print (df)
 
 print(person)
